1ab2c9fe938c54727595821cc96d6f6cc5b54869
yashpurkar <yashpurkar7079@gmail.com>
2023-01-25 18:33:50 +0530

    printed data using map

--- react-practical-questions/src/Axios/Posts.js
+++ react-practical-questions/src/Axios/Posts.js
@@ -3,15 +3,39 @@ import axios from 'axios';
 
 export default class Posts extends Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      users: []
+    }
+  }
+
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/users")
-      .then((resp) => console.log(resp.data.slice(0, 5))) // it takes (0 , n-1)
+      //.then((resp) => console.log(resp.data.slice(0, 5))) // it takes (0 , n-1)
+      .then((resp) => this.setState({ users: resp.data }))
       .catch((err) => console.log(err))
   }
 
   render() {
+    const { users } = this.state;
+    console.log(users)
+    // const users = this.state.users
     return (
-      <div>Posts</div>
+      <div>
+        {
+          users.map((user) => {
+            return (
+              <div key={user.id}>
+                <p>{user.name}</p>
+                <small>{user.email}</small>
+                <hr />
+              </div>
+            )
+          })
+        }
+      </div>
     )
   }
 }
